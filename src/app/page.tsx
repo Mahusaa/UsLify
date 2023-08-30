ec69677f41de4c04d18233f299086cecaa6045d0
usamah <hakaro375@gmail.com>
2023-08-30 12:43:30 +0700

    feat(button): add spinner in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import ButtonTest from '@/components/buttontest';
 import { useState } from 'react'
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   const [isLoading, setIsLoading ] = useState<boolean>(false);
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <ButtonTest isLoading={isLoading} onClick={clickHandler}>Click</ButtonTest>
+        <Button isLoading={isLoading} onClick={clickHandler}>Click</Button>
       </div>
     </main>
   )
